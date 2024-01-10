b924ee0a6ddf3845d49b236433937c4a50e53f72
Tania Sasaran <taniasasaran@gmail.com>
2024-01-10 19:20:09 +0200

    working example for g1 and simple if(g2)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String filename = "resources/g2short.txt"; // Syntax.in from GitHub 1b
+        String source = "resources/PIF.out"; // pif from GitHub lab5
 //        String filename = "resources/g1.txt"; // grammar from seminary
+//        String source = "resources/seq.txt"; // sequence from seminary
 //        Grammar grammar = new Grammar(filename);
 //        grammar.printProductions();
 //        System.out.println();
@@ -24,14 +28,28 @@ public class Main {
 
 
         LR0Parser lr0Parser = new LR0Parser(lrTable);
-        // IF 2 > 1 { WRITE(2) } ELSE {WRITE(1)}
-        ArrayList<Integer> parseResult = lr0Parser.parse(new ArrayList<>(Arrays.asList("IF", "2", ">", "1", "{", "WRITE", "(", "2", ")", "}", "ELSE", "{", "WRITE", "(", "1", ")", "}")));
-        StringBuilder stringResult = new StringBuilder();
+        // IF (2 > 1) { WRITE(2); } ELSE {WRITE(1);}
+        ArrayList<Integer> parseResult = lr0Parser.parse(getTokens(source));
         for (Integer i : parseResult){
             System.out.println(i);
-            stringResult.append(i);
         }
 
         lr0Parser.transformStringInTable(parseResult);
+    }
+
+    static ArrayList<String> getTokens(String source) {
+        ArrayList<String> tokens = new ArrayList<>();
+        // open file and read line by line
+        try {
+            Scanner scanner = new Scanner(new java.io.File(source));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] words = line.split(" ");
+                tokens.add(words[0]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tokens;
     }
 }
